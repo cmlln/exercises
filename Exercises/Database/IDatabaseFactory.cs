2c7e9af6e6326a71bf7891e9e31cc0288dad811f
Camilla <camilla.n94@gmail.com>
2026-08-10 11:32:12 +0200

    Corrections to database interface.

--- Exercises/Database/IDatabaseFactory.cs
+++ Exercises/Database/IDatabaseFactory.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Exercises.Database
 {
-    public interface DBConnection
+    public interface IDatabaseFactory
     {
-
+        IDbConnection CreateConnection();
     }
 }
