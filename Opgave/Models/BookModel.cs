d0f51f4d0ede6632cbafeea526bfb395e74ee204
Camilla <camilla.n94@gmail.com>
2026-09-23 15:26:31 +0200

    Exercise with models and dropdown.

---
+++ Opgave/Models/BookModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Opgave.Models
+{
+    public class BookModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1, 5000)]
+        public int Pages { get; set; }
+        public BookGenre Genre { get; set; }
+    }
+    public enum BookGenre
+    {
+        Thriller,
+        Horror,
+        Slasher,
+        Bodyhorror,
+        Monster,
+        Psychological,
+        Supernatural
+    }
+}
